a63c7f82ef61e7c493fe76f31d6e57c0f3b53b26
iadx7 <155528761+iadx7@users.noreply.github.com>
2024-01-03 12:07:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const mobileToggle = document.getElementById("mobile-toggle");
+
+mobileToggle.addEventListener("click", () => {
+  document.querySelector(".nav-items").classList.toggle("active");
+});
+
+const navLinks = document.querySelectorAll(".nav-item").forEach((navItem) => {
+  navItem.addEventListener("click", () => {
+    document.querySelector(".nav-items").classList.toggle("active");
+  });
+});
+
+document.getElementById("submit-btn").addEventListener("click", function () {
+  sendToChatGPT();
+});
+
+
+function sendToChatGPT() {
+  let value = document.getElementById("word-input").value;
+
+  let body = {
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: value }],
+    tempreture: "1",
+  };
+
+  let headers = {
+    Authorization: "Bearer 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",
+  };
+
+  axios
+    .post("https://api.openai.com/v1/chat/completions", body, {
+      headers: headers,
+    })
+    .then((response) => {
+      let reply = response.data.choices[0].message.content;
+      document.getElementById("reply-content").textContent = reply;
+    });
+}
